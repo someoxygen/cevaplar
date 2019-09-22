42423876095f30352687924fc2f272b47c006183
someoxygen <mustafaycl37@gmail.com>
2019-09-22 18:28:06 +0300

    Add files via upload

---
+++ cevaplar/FiveFunctionality/FiveFunctionality/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace FiveFunctionality
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstFuncTxtBox1 = new System.Windows.Forms.TextBox();
+            this.firstFuncLabel1 = new System.Windows.Forms.Label();
+            this.firstFuncTxtBox2 = new System.Windows.Forms.TextBox();
+            this.firstFuncTxtBox3 = new System.Windows.Forms.TextBox();
+            this.firstFuncTxtBox4 = new System.Windows.Forms.TextBox();
+            this.firstFuncLabel2 = new System.Windows.Forms.Label();
+            this.firstFuncLabel3 = new System.Windows.Forms.Label();
+            this.firstFunctionButton1 = new System.Windows.Forms.Button();
+            this.fifthFuncTxtBox1 = new System.Windows.Forms.TextBox();
+            this.fifthFuncBotton1 = new System.Windows.Forms.Button();
+            this.fifthFuncTxtBox2 = new System.Windows.Forms.TextBox();
+            this.fifthFuncLabel1 = new System.Windows.Forms.Label();
+            this.fifthFuncLabel2 = new System.Windows.Forms.Label();
+            this.SecondFuncButton1 = new System.Windows.Forms.Button();
+            this.SecondFuncLabel1 = new System.Windows.Forms.Label();
+            this.SecondFuncTxtBox1 = new System.Windows.Forms.TextBox();
+            this.thirdFuncLabel1 = new System.Windows.Forms.Label();
+            this.thirdFuncButton1 = new System.Windows.Forms.Button();
+            this.thirdFuncLabel2 = new System.Windows.Forms.Label();
+            this.thirdFuncTxtBox1 = new System.Windows.Forms.TextBox();
+            this.thirdFuncTxtBox2 = new System.Windows.Forms.TextBox();
+            this.fourthFuncLabel1 = new System.Windows.Forms.Label();
+            this.fourthFuncLabel2 = new System.Windows.Forms.Label();
+            this.fourthFuncButton1 = new System.Windows.Forms.Button();
+            this.fourthFuncTxtBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // firstFuncTxtBox1
+            // 
+            this.firstFuncTxtBox1.Location = new System.Drawing.Point(24, 30);
+            this.firstFuncTxtBox1.Name = "firstFuncTxtBox1";
+            this.firstFuncTxtBox1.Size = new System.Drawing.Size(38, 20);
+            this.firstFuncTxtBox1.TabIndex = 0;
+            this.firstFuncTxtBox1.Text = "0";
+            // 
+            // firstFuncLabel1
+            // 
+            this.firstFuncLabel1.AutoSize = true;
+            this.firstFuncLabel1.Location = new System.Drawing.Point(21, 9);
+            this.firstFuncLabel1.Name = "firstFuncLabel1";
+            this.firstFuncLabel1.Size = new System.Drawing.Size(67, 13);
+            this.firstFuncLabel1.TabIndex = 1;
+            this.firstFuncLabel1.Text = "FirstFunction";
+            this.firstFuncLabel1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // firstFuncTxtBox2
+            // 
+            this.firstFuncTxtBox2.Location = new System.Drawing.Point(87, 30);
+            this.firstFuncTxtBox2.Name = "firstFuncTxtBox2";
+            this.firstFuncTxtBox2.Size = new System.Drawing.Size(38, 20);
+            this.firstFuncTxtBox2.TabIndex = 2;
+            this.firstFuncTxtBox2.Text = "0";
+            // 
+            // firstFuncTxtBox3
+            // 
+            this.firstFuncTxtBox3.Location = new System.Drawing.Point(148, 30);
+            this.firstFuncTxtBox3.Name = "firstFuncTxtBox3";
+            this.firstFuncTxtBox3.Size = new System.Drawing.Size(38, 20);
+            this.firstFuncTxtBox3.TabIndex = 3;
+            this.firstFuncTxtBox3.Text = "0";
+            this.firstFuncTxtBox3.TextChanged += new System.EventHandler(this.FirstFuncTxtBox3_TextChanged);
+            // 
+            // firstFuncTxtBox4
+            // 
+            this.firstFuncTxtBox4.Location = new System.Drawing.Point(317, 29);
+            this.firstFuncTxtBox4.Name = "firstFuncTxtBox4";
+            this.firstFuncTxtBox4.Size = new System.Drawing.Size(38, 20);
+            this.firstFuncTxtBox4.TabIndex = 4;
+            this.firstFuncTxtBox4.Text = "0";
+            // 
+            // firstFuncLabel2
+            // 
+            this.firstFuncLabel2.AutoSize = true;
+            this.firstFuncLabel2.Location = new System.Drawing.Point(68, 33);
+            this.firstFuncLabel2.Name = "firstFuncLabel2";
+            this.firstFuncLabel2.Size = new System.Drawing.Size(13, 13);
+            this.firstFuncLabel2.TabIndex = 5;
+            this.firstFuncLabel2.Text = "+";
+            // 
+            // firstFuncLabel3
+            // 
+            this.firstFuncLabel3.AutoSize = true;
+            this.firstFuncLabel3.Location = new System.Drawing.Point(131, 34);
+            this.firstFuncLabel3.Name = "firstFuncLabel3";
+            this.firstFuncLabel3.Size = new System.Drawing.Size(12, 13);
+            this.firstFuncLabel3.TabIndex = 6;
+            this.firstFuncLabel3.Text = "x";
+            // 
+            // firstFunctionButton1
+            // 
+            this.firstFunctionButton1.Location = new System.Drawing.Point(192, 27);
+            this.firstFunctionButton1.Name = "firstFunctionButton1";
+            this.firstFunctionButton1.Size = new System.Drawing.Size(119, 23);
+            this.firstFunctionButton1.TabIndex = 8;
+            this.firstFunctionButton1.Text = "Sonucu Göster";
+            this.firstFunctionButton1.UseVisualStyleBackColor = true;
+            this.firstFunctionButton1.Click += new System.EventHandler(this.FirstFunctionButton1_Click);
+            // 
+            // fifthFuncTxtBox1
+            // 
+            this.fifthFuncTxtBox1.Location = new System.Drawing.Point(468, 27);
+            this.fifthFuncTxtBox1.Name = "fifthFuncTxtBox1";
+            this.fifthFuncTxtBox1.Size = new System.Drawing.Size(59, 20);
+            this.fifthFuncTxtBox1.TabIndex = 9;
+            this.fifthFuncTxtBox1.Text = "0";
+            // 
+            // fifthFuncBotton1
+            // 
+            this.fifthFuncBotton1.Location = new System.Drawing.Point(549, 24);
+            this.fifthFuncBotton1.Name = "fifthFuncBotton1";
+            this.fifthFuncBotton1.Size = new System.Drawing.Size(119, 23);
+            this.fifthFuncBotton1.TabIndex = 10;
+            this.fifthFuncBotton1.Text = "Sonucu Göster";
+            this.fifthFuncBotton1.UseVisualStyleBackColor = true;
+            this.fifthFuncBotton1.Click += new System.EventHandler(this.FifthFuncBotton1_Click);
+            // 
+            // fifthFuncTxtBox2
+            // 
+            this.fifthFuncTxtBox2.Location = new System.Drawing.Point(688, 27);
+            this.fifthFuncTxtBox2.Name = "fifthFuncTxtBox2";
+            this.fifthFuncTxtBox2.Size = new System.Drawing.Size(100, 20);
+            this.fifthFuncTxtBox2.TabIndex = 11;
+            this.fifthFuncTxtBox2.Text = "0";
+            // 
+            // fifthFuncLabel1
+            // 
+            this.fifthFuncLabel1.AutoSize = true;
+            this.fifthFuncLabel1.Location = new System.Drawing.Point(468, 8);
+            this.fifthFuncLabel1.Name = "fifthFuncLabel1";
+            this.fifthFuncLabel1.Size = new System.Drawing.Size(68, 13);
+            this.fifthFuncLabel1.TabIndex = 12;
+            this.fifthFuncLabel1.Text = "FifthFunction";
+            // 
+            // fifthFuncLabel2
+            // 
+            this.fifthFuncLabel2.AutoSize = true;
+            this.fifthFuncLabel2.Location = new System.Drawing.Point(389, 30);
+            this.fifthFuncLabel2.Name = "fifthFuncLabel2";
+            this.fifthFuncLabel2.Size = new System.Drawing.Size(59, 13);
+            this.fifthFuncLabel2.TabIndex = 13;
+            this.fifthFuncLabel2.Text = "Fibonacci :";
+            // 
+            // SecondFuncButton1
+            // 
+            this.SecondFuncButton1.Location = new System.Drawing.Point(24, 78);
+            this.SecondFuncButton1.Name = "SecondFuncButton1";
+            this.SecondFuncButton1.Size = new System.Drawing.Size(119, 23);
+            this.SecondFuncButton1.TabIndex = 14;
+            this.SecondFuncButton1.Text = "Sonucu Göster";
+            this.SecondFuncButton1.UseVisualStyleBackColor = true;
+            this.SecondFuncButton1.Click += new System.EventHandler(this.SecondFuncButton1_Click);
+            // 
+            // SecondFuncLabel1
+            // 
+            this.SecondFuncLabel1.AutoSize = true;
+            this.SecondFuncLabel1.Location = new System.Drawing.Point(21, 62);
+            this.SecondFuncLabel1.Name = "SecondFuncLabel1";
+            this.SecondFuncLabel1.Size = new System.Drawing.Size(85, 13);
+            this.SecondFuncLabel1.TabIndex = 15;
+            this.SecondFuncLabel1.Text = "SecondFunction";
+            // 
+            // SecondFuncTxtBox1
+            // 
+            this.SecondFuncTxtBox1.Location = new System.Drawing.Point(24, 107);
+            this.SecondFuncTxtBox1.Multiline = true;
+            this.SecondFuncTxtBox1.Name = "SecondFuncTxtBox1";
+            this.SecondFuncTxtBox1.Size = new System.Drawing.Size(775, 91);
+            this.SecondFuncTxtBox1.TabIndex = 16;
+            // 
+            // thirdFuncLabel1
+            // 
+            this.thirdFuncLabel1.AutoSize = true;
+            this.thirdFuncLabel1.Location = new System.Drawing.Point(21, 210);
+            this.thirdFuncLabel1.Name = "thirdFuncLabel1";
+            this.thirdFuncLabel1.Size = new System.Drawing.Size(72, 13);
+            this.thirdFuncLabel1.TabIndex = 17;
+            this.thirdFuncLabel1.Text = "ThirdFunction";
+            // 
+            // thirdFuncButton1
+            // 
+            this.thirdFuncButton1.Location = new System.Drawing.Point(149, 227);
+            this.thirdFuncButton1.Name = "thirdFuncButton1";
+            this.thirdFuncButton1.Size = new System.Drawing.Size(119, 23);
+            this.thirdFuncButton1.TabIndex = 18;
+            this.thirdFuncButton1.Text = "Sonucu Göster";
+            this.thirdFuncButton1.UseVisualStyleBackColor = true;
+            this.thirdFuncButton1.Click += new System.EventHandler(this.ThirdFuncButton1_Click);
+            // 
+            // thirdFuncLabel2
+            // 
+            this.thirdFuncLabel2.AutoSize = true;
+            this.thirdFuncLabel2.Location = new System.Drawing.Point(27, 233);
+            this.thirdFuncLabel2.Name = "thirdFuncLabel2";
+            this.thirdFuncLabel2.Size = new System.Drawing.Size(30, 13);
+            this.thirdFuncLabel2.TabIndex = 19;
+            this.thirdFuncLabel2.Text = "Sayı:";
+            // 
+            // thirdFuncTxtBox1
+            // 
+            this.thirdFuncTxtBox1.Location = new System.Drawing.Point(63, 230);
+            this.thirdFuncTxtBox1.Name = "thirdFuncTxtBox1";
+            this.thirdFuncTxtBox1.Size = new System.Drawing.Size(80, 20);
+            this.thirdFuncTxtBox1.TabIndex = 20;
+            // 
+            // thirdFuncTxtBox2
+            // 
+            this.thirdFuncTxtBox2.Location = new System.Drawing.Point(24, 256);
+            this.thirdFuncTxtBox2.Multiline = true;
+            this.thirdFuncTxtBox2.Name = "thirdFuncTxtBox2";
+            this.thirdFuncTxtBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.thirdFuncTxtBox2.Size = new System.Drawing.Size(769, 228);
+            this.thirdFuncTxtBox2.TabIndex = 21;
+            // 
+            // fourthFuncLabel1
+            // 
+            this.fourthFuncLabel1.AutoSize = true;
+            this.fourthFuncLabel1.Location = new System.Drawing.Point(21, 496);
+            this.fourthFuncLabel1.Name = "fourthFuncLabel1";
+            this.fourthFuncLabel1.Size = new System.Drawing.Size(78, 13);
+            this.fourthFuncLabel1.TabIndex = 22;
+            this.fourthFuncLabel1.Text = "FourthFunction";
+            // 
+            // fourthFuncLabel2
+            // 
+            this.fourthFuncLabel2.AutoSize = true;
+            this.fourthFuncLabel2.Location = new System.Drawing.Point(21, 522);
+            this.fourthFuncLabel2.Name = "fourthFuncLabel2";
+            this.fourthFuncLabel2.Size = new System.Drawing.Size(62, 13);
+            this.fourthFuncLabel2.TabIndex = 23;
+            this.fourthFuncLabel2.Text = "Dosya Seç:";
+            // 
+            // fourthFuncButton1
+            // 
+            this.fourthFuncButton1.Location = new System.Drawing.Point(89, 517);
+            this.fourthFuncButton1.Name = "fourthFuncButton1";
+            this.fourthFuncButton1.Size = new System.Drawing.Size(75, 23);
+            this.fourthFuncButton1.TabIndex = 24;
+            this.fourthFuncButton1.Text = "Dosya Seç";
+            this.fourthFuncButton1.UseVisualStyleBackColor = true;
+            this.fourthFuncButton1.Click += new System.EventHandler(this.FourthFuncButton1_Click);
+            // 
+            // fourthFuncTxtBox1
+            // 
+            this.fourthFuncTxtBox1.Location = new System.Drawing.Point(22, 546);
+            this.fourthFuncTxtBox1.Multiline = true;
+            this.fourthFuncTxtBox1.Name = "fourthFuncTxtBox1";
+            this.fourthFuncTxtBox1.Size = new System.Drawing.Size(766, 73);
+            this.fourthFuncTxtBox1.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(918, 650);
+            this.Controls.Add(this.fourthFuncTxtBox1);
+            this.Controls.Add(this.fourthFuncButton1);
+            this.Controls.Add(this.fourthFuncLabel2);
+            this.Controls.Add(this.fourthFuncLabel1);
+            this.Controls.Add(this.thirdFuncTxtBox2);
+            this.Controls.Add(this.thirdFuncTxtBox1);
+            this.Controls.Add(this.thirdFuncLabel2);
+            this.Controls.Add(this.thirdFuncButton1);
+            this.Controls.Add(this.thirdFuncLabel1);
+            this.Controls.Add(this.SecondFuncTxtBox1);
+            this.Controls.Add(this.SecondFuncLabel1);
+            this.Controls.Add(this.SecondFuncButton1);
+            this.Controls.Add(this.fifthFuncLabel2);
+            this.Controls.Add(this.fifthFuncLabel1);
+            this.Controls.Add(this.fifthFuncTxtBox2);
+            this.Controls.Add(this.fifthFuncBotton1);
+            this.Controls.Add(this.fifthFuncTxtBox1);
+            this.Controls.Add(this.firstFunctionButton1);
+            this.Controls.Add(this.firstFuncLabel3);
+            this.Controls.Add(this.firstFuncLabel2);
+            this.Controls.Add(this.firstFuncTxtBox4);
+            this.Controls.Add(this.firstFuncTxtBox3);
+            this.Controls.Add(this.firstFuncTxtBox2);
+            this.Controls.Add(this.firstFuncLabel1);
+            this.Controls.Add(this.firstFuncTxtBox1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstFuncTxtBox1;
+        private System.Windows.Forms.Label firstFuncLabel1;
+        private System.Windows.Forms.TextBox firstFuncTxtBox2;
+        private System.Windows.Forms.TextBox firstFuncTxtBox3;
+        private System.Windows.Forms.TextBox firstFuncTxtBox4;
+        private System.Windows.Forms.Label firstFuncLabel2;
+        private System.Windows.Forms.Label firstFuncLabel3;
+        private System.Windows.Forms.Button firstFunctionButton1;
+        private System.Windows.Forms.TextBox fifthFuncTxtBox1;
+        private System.Windows.Forms.Button fifthFuncBotton1;
+        private System.Windows.Forms.TextBox fifthFuncTxtBox2;
+        private System.Windows.Forms.Label fifthFuncLabel1;
+        private System.Windows.Forms.Label fifthFuncLabel2;
+        private System.Windows.Forms.Button SecondFuncButton1;
+        private System.Windows.Forms.Label SecondFuncLabel1;
+        private System.Windows.Forms.TextBox SecondFuncTxtBox1;
+        private System.Windows.Forms.Label thirdFuncLabel1;
+        private System.Windows.Forms.Button thirdFuncButton1;
+        private System.Windows.Forms.Label thirdFuncLabel2;
+        private System.Windows.Forms.TextBox thirdFuncTxtBox1;
+        private System.Windows.Forms.TextBox thirdFuncTxtBox2;
+        private System.Windows.Forms.Label fourthFuncLabel1;
+        private System.Windows.Forms.Label fourthFuncLabel2;
+        private System.Windows.Forms.Button fourthFuncButton1;
+        private System.Windows.Forms.TextBox fourthFuncTxtBox1;
+    }
+}
+
---
+++ cevaplar/FiveFunctionality/FiveFunctionality/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiveFunctionality
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FirstFuncTxtBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FirstFunctionButton1_Click(object sender, EventArgs e)
+        {
+            firstFuncTxtBox4.Text = Convert.ToString((Convert.ToDouble(firstFuncTxtBox1.Text.ToString()) + Convert.ToDouble(firstFuncTxtBox2.Text.ToString())) * Convert.ToDouble(firstFuncTxtBox3.Text.ToString()));
+        }
+
+        private void FifthFuncBotton1_Click(object sender, EventArgs e)
+        {
+            string fibonacci = "";
+            int number1 = 1;
+            int number2 = 1;
+            int fibonacciResult = 0;
+            fibonacci = fifthFuncTxtBox1.Text.ToString();
+            int fibonacciSayisi = Convert.ToInt32(fibonacci);
+
+            if (fibonacciSayisi == 1)
+            {
+                fibonacciResult = 0;
+            }
+            else if (fibonacciSayisi == 2)
+            {
+                fibonacciResult = 1;
+            }
+            else if (fibonacciSayisi == 3)
+            {
+                fibonacciResult = 1;
+            }
+            else
+            {
+                for (int i = 3; i < fibonacciSayisi; i++)
+                {
+                        fibonacciResult = number1 + number2;
+                        number1 = number2;
+                        number2 = fibonacciResult;
+                }
+            }
+                
+            
+            fifthFuncTxtBox2.Text = Convert.ToString(fibonacciResult);
+
+        }
+
+        private void SecondFuncButton1_Click(object sender, EventArgs e)
+        {
+            for(int i = 1; i <= 200; i++)
+            {
+                
+                if (i % 3 == 0 && i % 5 == 0&&i>=100)
+                {
+                    SecondFuncTxtBox1.Text += "zagzig  ";
+                    continue;
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    SecondFuncTxtBox1.Text += "zigzag  ";
+                    continue;
+                }
+
+                if(i % 3 == 0)
+                {
+                    SecondFuncTxtBox1.Text += "zig  ";
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    SecondFuncTxtBox1.Text += "zag  ";
+                    continue;
+                }
+                
+
+                SecondFuncTxtBox1.Text += i + "  ";
+
+            }
+        }
+
+        private void ThirdFuncButton1_Click(object sender, EventArgs e)
+        {
+            string thirdFuncString = thirdFuncTxtBox1.Text.ToString();
+            int thirdFuncSayac = Convert.ToInt32(thirdFuncString);
+            thirdFuncTxtBox2.Clear();
+            if (thirdFuncSayac > 15)
+            {
+                thirdFuncTxtBox2.Text = "Lütfen 1 den 15 e kadar bir tam sayı giriniz...";
+            }
+            else
+            {
+                for (int i = 0; i <= thirdFuncSayac; i++)
+                {
+
+                    for (int j = 0; j <= thirdFuncSayac; j++)
+                    {
+                        if (i * j >= 100)
+                        {
+                            thirdFuncTxtBox2.Text += String.Format("{0,6}", i * j);
+                            //thirdFuncTxtBox2.Text += i * j + "";
+                            continue;
+                        }
+                        if (i * j >= 10)
+                        {
+                            thirdFuncTxtBox2.Text += String.Format("{0,7}", i * j);
+                            //thirdFuncTxtBox2.Text += i * j + "  ";
+                            continue;
+                        }
+                        if (j == 0 && i != 0)
+                        {
+                            if (i >= 10)
+                            {
+                                thirdFuncTxtBox2.Text += String.Format("{0,7}", i);
+                            }
+                            else
+                            {
+                                thirdFuncTxtBox2.Text += String.Format("{0,8}", i);
+                            }
+                            
+
+                            //thirdFuncTxtBox2.Text += i + "    ";
+                            continue;
+                        }
+                        if (i == 0)
+                        {
+                            if (j >= 10)
+                            {
+                                thirdFuncTxtBox2.Text += String.Format("{0,7}", j);
+                            }
+                            else
+                            {
+                                thirdFuncTxtBox2.Text += String.Format("{0,8}", j);
+                            }
+                            
+                            //thirdFuncTxtBox2.Text += j + "    ";
+                            continue;
+                        }
+                        
+                        
+                        thirdFuncTxtBox2.Text += String.Format("{0,8}",i*j);
+                        //thirdFuncTxtBox2.Text += i * j + "    ";
+                    }
+
+                    thirdFuncTxtBox2.AppendText(Environment.NewLine);
+                }
+            }
+            
+
+        } 
+
+        private void FourthFuncButton1_Click(object sender, EventArgs e)
+        {
+            fourthFuncTxtBox1.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string directoryFile = string.Empty;
+            string fileName = string.Empty;
+            openFileDialog.Filter = "Text File | *.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                directoryFile = openFileDialog.FileName;
+                fileName = openFileDialog.SafeFileName;
+                string file = directoryFile;
+                FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+
+                StreamReader streamReader = new StreamReader(fileStream);
+                string words = streamReader.ReadToEnd();
+                string wordsTrim = Regex.Replace(words, @"s+", "").Trim();
+                wordsTrim = wordsTrim.Replace('\t', 'x');
+                wordsTrim = wordsTrim.Replace('\n', 'x');
+                wordsTrim = wordsTrim.Replace('\r', 'x');
+                wordsTrim = wordsTrim.Replace(' ' , 'x');
+                wordsTrim = wordsTrim.Replace("xx","x");
+                wordsTrim = wordsTrim.Replace("xxx", "x");
+                wordsTrim = wordsTrim.Replace("xxxx", "x");
+                wordsTrim = wordsTrim.Replace("xxxxx", "x");
+                wordsTrim = wordsTrim.Replace("xx", "x");
+                char [] hole = {'x'};
+                string[] wordsSplit = wordsTrim.Split(hole);
+                double[] value = new double[wordsSplit.Length];
+                for(int i = 0; i < wordsSplit.Length; i++)
+                {
+                    double.TryParse(wordsSplit[i], out value[i]);
+                    
+                }
+                double tempValue = 0;
+                for (int j = 0; j < wordsSplit.Length-1; j++)
+                {
+                    for (int k = 1; k < wordsSplit.Length-j; k++)
+                    {
+                        if (value[k] > value[k - 1])
+                        {
+                            tempValue = value[k - 1];
+                            value[k - 1] = value[k];
+                            value[k] = tempValue;
+                        }
+                    }
+                }
+                for(int t = 0; t < wordsSplit.Length; t++)
+                {
+                    fourthFuncTxtBox1.Text += value[t] + "   ";
+                }
+
+                streamReader.Close();
+                fileStream.Close();
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
